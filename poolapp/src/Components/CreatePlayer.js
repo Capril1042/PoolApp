73dfd30c4feb057cc3eab0b1fbf983ea0fbf660a
Capril1042 <Caprilewis1042@gmail.com>
2019-07-18 21:48:30 -0400

    sets up firebase and adds form for creating a player

--- poolapp/src/Components/CreatePlayer.js
+++ poolapp/src/Components/CreatePlayer.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import db from "../firebase.js";
 
 import "./CreatePlayer.css";
 
 class CreatePlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "" };
+  }
+
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let newName = this.state.name;
+
+    db.collection("players").add({
+      name: newName
+    });
+    alert(`${this.state.name} was added`);
+    this.setState({ name: "" });
+  };
+
   render() {
-    return <div className="CreatePlayer">CreatePlayer</div>;
+    return (
+      <div className="CreatePlayer">
+        <h1>Create Player</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={event => this.handleChange(event)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
---
+++ poolapp/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("dotenv").config();
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: "poolapp-cbd3a",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+const Firebase = firebase.initializeApp(config);
+const db = Firebase.firestore();
+
+export default db;
