601f9fed112bf82533c35237abfb12d03fc9f00b
Capril1042 <Caprilewis1042@gmail.com>
2019-07-19 15:49:13 -0400

    WIP create Game

--- poolapp/src/Components/CreateGame.js
+++ poolapp/src/Components/CreateGame.js
@@ -9,9 +9,30 @@ class CreateGame extends Component {
     this.state = {
       players: [],
       playerOne: "",
-      playerTwo: ""
+      playerTwo: "",
+      winner: ""
     };
   }
+
+  handleChangePlayerOne = e => {
+    e.preventDefault();
+    this.setState({ playerOne: e.target.value });
+  };
+
+  handleChangePlayerTwo = e => {
+    e.preventDefault();
+    this.setState({ playerTwo: e.target.value });
+  };
+
+  handleChangeWinner = e => {
+    e.preventDefault();
+    this.setState({ winner: e.target.value });
+  };
+
+  handleSubmit = e => {
+    //save game document to firebase
+    e.preventDefault();
+  };
 
   componentDidMount() {
     db.collection("players")
@@ -30,31 +51,57 @@ class CreateGame extends Component {
 
   render() {
     const players = this.state.players;
-    console.log(players);
+    console.log(this.state.playerOne);
+    console.log(this.state.playerTwo);
+    console.log(this.state.winner);
     return (
       <div className="CreateGame">
         <h1>Create Game</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Player 1:
-            <select>
+            <select
+              type="select"
+              name="playerOne"
+              onChange={this.handleChangePlayerOne}
+            >
               {players.map((player, i) => (
                 <option key={i} value={player.data.name}>
                   {player.data.name}
                 </option>
-              ))};
+              ))}
+              ;
             </select>
           </label>
           <label>
-            Player 2:<select />
+            Player 2:
+            <select
+              type="select"
+              name="playerTwo"
+              onChange={this.handleChangePlayerTwo}
+            >
+              {players
+                .filter(player => player.data.name !== this.state.playerOne)
+                .map((player, i) => (
+                  <option key={i} value={player.data.name}>
+                    {player.data.name}
+                  </option>
+                ))}
+              ;
+            </select>
           </label>
           <label>
             winner:
-            <select>
-              <option>{this.state.player1}</option>
-              <option>{this.state.player2}</option>
+            <select type="select" name="winner" onChange={this.handleWinner}>
+              <option value={this.state.playerOne}>
+                {this.state.playerOne}
+              </option>
+              <option value={this.state.playerTwo}>
+                {this.state.playerTwo}
+              </option>
             </select>
           </label>
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
