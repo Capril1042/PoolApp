229524dc4589e81718290fb0de279e5f2a6ea72a
Capril1042 <Caprilewis1042@gmail.com>
2019-07-21 02:59:04 -0400

    Adds leaderboard and PlayersWIn function

--- poolapp/src/Components/Leaderboard.js
+++ poolapp/src/Components/Leaderboard.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
-
+import playersWins from "../Utils/PlayersWins.js";
+import db from "../firebase.js";
 import "./Leaderboard.css";
 
 class Leaderboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: []
+    };
+  }
+  componentDidMount() {
+    db.collection("games")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.setState(({ games }) => ({
+            games: [...games, { id: doc.id, data: doc.data() }]
+          }));
+        });
+      })
+      .catch(err => {
+        console.log("Error getting documents", err);
+      });
+  }
   render() {
-    return <div className="Leaderboard">Leaderboard</div>;
+    const scores = playersWins(this.state.games);
+    console.log(scores);
+    return (
+      <div className="Leaderboard">
+        {" "}
+        Leaderboard
+        <table>
+          <thead>
+            {" "}
+            <tr>
+              <th>Name</th>
+              <th> Wins</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {scores.map((winner, i) => (
+              <tr key={i}>
+                <td>{winner.name}</td>
+                <td>{winner.wins}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
--- poolapp/src/Utils/PlayersWins.js
+++ poolapp/src/Utils/PlayersWins.js
@@ -1,8 +1,17 @@
 export default function playersWins(gamesArray) {
-    const winners =[];
-     gamesArray.forEach(game=> winners.push(game.data.winner));
-    const winnersCount = new Map([...new Set(winners)]
-    .map(x => [x, winners.filter(y => y === x).length]
-        ));
-    return winnersCount;
+  let playersWithWinSum = new Map();
+  let WinnerSumArray = [];
+  gamesArray.forEach(game => {
+    if (playersWithWinSum.has(game.data.winner)) {
+      let newWinTotal = playersWithWinSum.get(game.data.winner) + 1;
+      playersWithWinSum.set(game.data.winner, newWinTotal);
+    } else {
+      playersWithWinSum.set(game.data.winner, 1);
+    }
+  });
+
+  for (const [key, value] of playersWithWinSum.entries()) {
+    WinnerSumArray.push({ name: key, wins: value });
+  }
+  return WinnerSumArray.sort((a, b) => b.wins - a.wins);
 }
