2ebeb77fcc188460b862236a06784d01d5cfd9f7
Capril1042 <Caprilewis1042@gmail.com>
2019-07-21 13:59:37 -0400

    styles landing page

--- poolapp/src/Components/Landing.js
+++ poolapp/src/Components/Landing.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./Landing.css";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="Landing">
-        <h1 className="Landing__Header">Pool App</h1>
-        <Link to="/createplayer">create player</Link>
-        <Link to="/creategame"> create game</Link>
-        <Link to="/leaderboard"> leaderboard </Link>
+      <div className="landing">
+        <h1 className="landing--header">Pool App</h1>
+        <div className="landing--buttons">
+          <button className="button-big">
+            <Link to="/createplayer">ADD PLAYER</Link>
+          </button>
+          <button className="button--big">
+            <Link to="/creategame"> ADD GAME</Link>
+          </button>
+          <button className="button--big">
+            <Link to="/leaderboard"> LEADERBOARD </Link>
+          </button>
+        </div>
       </div>
     );
   }
