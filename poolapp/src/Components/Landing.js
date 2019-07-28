5a075291a72182ad7dcded1fbc78e62d4e32427f
Capril1042 <Caprilewis1042@gmail.com>
2019-07-28 11:07:36 -0400

    refactors Landing adds linkbutton component

--- poolapp/src/Components/Landing.js
+++ poolapp/src/Components/Landing.js
@@ -1,26 +1,18 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import LinkButton from "./LinkButton";
 import "./Landing.css";
 
-class Landing extends Component {
-  render() {
-    return (
-      <div className="landing">
-        <h1 className="landing--header">Pool App</h1>
-        <div className="landing--buttons">
-          <button className="button-big">
-            <Link to="/createplayer">ADD PLAYER</Link>
-          </button>
-          <button className="button--big">
-            <Link to="/creategame"> ADD GAME</Link>
-          </button>
-          <button className="button--big">
-            <Link to="/leaderboard"> LEADERBOARD </Link>
-          </button>
-        </div>
+function Landing() {
+  return (
+    <div className="landing">
+      <h1 className="landing--header">Pool App</h1>
+      <div className="landing--buttons">
+        <LinkButton route={"/createplayer"} text={"ADD PLAYER"} />
+        <LinkButton route={"/creategame"} text={"ADD Game"} />
+        <LinkButton route={"/leaderboard"} text={"LEADERBOARD"} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Landing;
---
+++ poolapp/src/Components/LinkButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function LinkButton(props) {
+  return (
+    <div className="linkbutton">
+      <button className="button-big">
+          <Link to={props.route}>{props.text}</Link>
+        </button>
+    </div>
+  );
+}
+
+export default LinkButton;
